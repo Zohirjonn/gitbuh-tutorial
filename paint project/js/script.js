39ee957f48e1e2008617329e860a0ca492196a98
zohijonn <example@gmail.com>
2022-12-19 11:07:00 +0500

    add restangle strokerest

--- paint project/js/script.js
+++ paint project/js/script.js
@@ -1,28 +1,71 @@
-const canvas = document.querySelector("canvas");
+//Global varibles
+const canvas = document.querySelector("canvas"),
+  toolBtns = document.querySelectorAll(".tool");
+// Varibles
 let ctx = canvas.getContext("2d"),
   isDrawing = false,
-  brushWidth = 5;
+  brushWidth = 5,
+  selectedTool = "",
+  prevMouseX,
+  prevmouseY,
+  snapshot;
+//Set canvas width end heigth
 window.addEventListener("load", () => {
   canvas.width = canvas.offsetWidth;
   canvas.height = canvas.offsetHeight;
 });
-const startDraw = () => {
+
+//Start drawing
+const startDraw = (e) => {
   isDrawing = true;
+  prevMouseX = e.offsetX;
+  prevmouseY = e.offsetY;
   ctx.beginPath(); //mishka uzilganda yangi joydan chizish
   ctx.lineWidth = brushWidth; // qalam qalinligi
+  snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
 };
+//End drawing
 const endDrawing = () => {
   isDrawing = false;
 };
+//Draw ractangle
+const drawRectangle = (e) => {
+  ctx.strokeRect(
+    //tortburchak chizish
+    e.offsetX,
+    e.offsetY,
+    prevMouseX - e.offsetX,
+    prevmouseY - e.offsetY
+  );
+};
+//Drawing
 const drawing = (e) => {
   if (!isDrawing) {
     return;
-  } else {
-    ctx.lineTo(e.offsetX, e.offsetY);
-    ctx.stroke(); //chizish
-    return;
+  }
+  ctx.putImageData(snapshot, 0, 0);
+  switch (selectedTool) {
+    case "brush":
+      ctx.lineTo(e.offsetX, e.offsetY);
+      ctx.stroke(); //chizish
+      break;
+    case "restangle":
+      drawRectangle(e);
+      break;
+    default:
+      break;
   }
 };
+//Tools Btn and to varibles selected  tool
+toolBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    // document.querySelector("#brush").classList.add("active  ");
+    document.querySelector(".options .active").classList.remove("active");
+    btn.classList.add("active");
+    selectedTool = btn.id;
+    console.log(selectedTool);
+  });
+});
 canvas.addEventListener("mousedown", startDraw);
 canvas.addEventListener("mouseup", endDrawing);
 canvas.addEventListener("mousemove", drawing);
